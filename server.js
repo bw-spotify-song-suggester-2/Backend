a33d48f74787c3eac1b7146d722587ca3c6f86ad
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-02-25 21:42:07 -0800

    Base setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
+
+module.exports = server;
